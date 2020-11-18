06b095acc5010b130571e2df76d11f8a5e8c5b07
Bryce Shang <shanggu@amazon.com>
2020-11-17 21:17:56 -0800

    Increase HMAC process limit.

--- SAW/proof/common/utility.go
+++ SAW/proof/common/utility.go
@@ -16,7 +16,7 @@ import (
 	"sync"
 )
 
-const HMAC_PROCESS_LIMIT int = 20
+const HMAC_PROCESS_LIMIT int = 40
 
 // A utility function to terminate this program when err exists.
 func CheckError(e error) {
