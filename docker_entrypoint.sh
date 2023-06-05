b65878bf98ee1ef345dffba74d949a2dda6947ee
Adam Petcher <apetcher@amazon.com>
2023-06-05 13:19:54 -0400

    opam initialization

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,4 +4,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 (pushd SAW; ./scripts/install.sh; ./scripts/entrypoint_check.sh; popd)
-(eval $(opam env); pushd Coq; make; popd)
+eval $(opam env)
+(pushd Coq; make; popd)
