ac3d3783c8558422859090fa2c5c2b66dff6d85e
Adam Petcher <apetcher@amazon.com>
2023-06-12 13:36:36 -0400

    Parallelizing build

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,9 +6,10 @@
 # The container runs as root, but the git checkout was performed by another user.
 # Disable git security checks that ensure files are owned by the current user.
 git config --global --add safe.directory '*'
+NUM_CPU_THREADS=$(grep -c ^processor /proc/cpuinfo)
 (pushd SAW; ./scripts/install.sh; popd)
 export CI=true
 export OPAMROOT=/root/.opam
 eval $(opam env)
-(pushd Coq; make; popd)
+(pushd Coq; make -j $NUM_CPU_THREADS; popd)
 (pushd SAW; ./scripts/entrypoint_check.sh; popd)
