7eee62029d3b257e3407203b54936b69e29817af
Adam Petcher <apetcher@amazon.com>
2023-06-12 12:17:49 -0400

    workflow/git troubleshooting.

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,7 +3,9 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-
+# The container runs as root, but the git checkout was performed by another user.
+# Disable git security checks that ensure files are owned by the current user.
+git config --global --add safe.directory '*'
 (pushd SAW; ./scripts/install.sh; popd)
 export PATH=$PATH:$HOME/bin
 export CI=true
