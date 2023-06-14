2b4fd69da8453a8c491e679729cb4872f9d0a480
Adam Petcher <apetcher@amazon.com>
2023-06-14 08:28:45 -0400

    Tweaking build and workflow

--- Coq/docker_entrypoint.sh
+++ Coq/docker_entrypoint.sh
@@ -6,11 +6,10 @@
 # The container runs as root, but the git checkout was performed by another user.
 # Disable git security checks that ensure files are owned by the current user.
 git config --global --add safe.directory '*'
-NUM_CPU_THREADS=$(grep -c ^processor /proc/cpuinfo)
-(pushd SAW; ./scripts/install.sh; popd)
+(pushd ../SAW; ./scripts/install.sh; popd)
 export CI=true
 export OPAMROOT=/root/.opam
 eval $(opam env)
+NUM_CPU_THREADS=$(grep -c ^processor /proc/cpuinfo)
 #(pushd Coq; make -j $NUM_CPU_THREADS; popd)
-(pushd Coq; make; popd)
-(pushd SAW; ./scripts/entrypoint_check.sh; popd)
+make
---
+++ SAW/scripts/docker_entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -ex
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+(./scripts/install.sh && ./scripts/entrypoint_check.sh)
