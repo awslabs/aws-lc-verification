d87215de582643268cf4af5154bb134eab2308da
Adam Petcher <apetcher@amazon.com>
2023-06-15 08:13:24 -0400

    build troubleshooting

--- SAW/scripts/release_jobs.sh
+++ SAW/scripts/release_jobs.sh
@@ -4,13 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # If |*_SELECTCHECK| env variable does not exist, run quick check of all algorithms.
-saw proof/SHA512/verify-SHA512-384.saw
-saw proof/SHA512/verify-SHA512-512.saw
-saw proof/HMAC/verify-HMAC.saw
-saw proof/KDF/verify-HKDF.saw
+#saw proof/SHA512/verify-SHA512-384.saw
+#saw proof/SHA512/verify-SHA512-512.saw
+#saw proof/HMAC/verify-HMAC.saw
+#saw proof/KDF/verify-HKDF.saw
 cd proof/AES && go run AES-GCM-check-entrypoint.go
 saw proof/AES_KW/verify-AES_KW.saw
 saw proof/AES_KW/verify-AES_KWP.saw
 saw proof/EC/verify-P384.saw
-saw proof/ECDSA/verify-ECDSA.saw
+#saw proof/ECDSA/verify-ECDSA.saw
 saw proof/ECDH/verify-ECDH.saw
