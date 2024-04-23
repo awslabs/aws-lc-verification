6b0749d0528d4c1492eeeaee61a30982c2457a44
Ryan Scott <ryan.gl.scott@gmail.com>
2024-04-23 10:18:54 -0400

    CI: Update SAW_URLs
    This updates the `SAW_URL` environment variables to point to a more recent
    version of SAW that includes the changes from
    https://github.com/GaloisInc/saw-script/pull/2037, which are necessary to
    support the AES-GCM proofs.

--- NSym/scripts/install.sh
+++ NSym/scripts/install.sh
@@ -5,7 +5,7 @@
 
 set -ex
 
-SAW_URL='https://saw-builds.s3.us-west-2.amazonaws.com/saw-0.9.0.99-2023-06-08-ab46c76e0-Linux-x86_64.tar.gz'
+SAW_URL='https://saw-builds.s3.us-west-2.amazonaws.com/saw-1.1.0.99-2024-04-23-48dcbf7bc-PR2037-Linux-x86_64.tar.gz'
 C2A_URL='https://cryptol-air-interface.s3.us-west-2.amazonaws.com/cryptol-air-interface-2023-11-20-fd3447e-Linux-x86_64.tar.gz'
 ELF_URL='https://ocaml-elf-loader.s3.us-west-2.amazonaws.com/elf_loader-2023-11-09-c95cf1c-Linux_x86_64.tar.gz'
 OSI_URL='https://ocaml-smt-interface.s3.us-west-2.amazonaws.com/ocaml_smt_interface-2023-11-07-9654c87-Linux_x86_64.tar.gz'
