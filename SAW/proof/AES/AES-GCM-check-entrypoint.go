0e07d32e842c073ad7feccebe8c7c30033dcedf3
Ryan Scott <ryan.gl.scott@gmail.com>
2024-05-02 13:23:33 -0400

    Review comments, part 2

--- SAW/proof/AES/AES-GCM-check-entrypoint.go
+++ SAW/proof/AES/AES-GCM-check-entrypoint.go
@@ -28,9 +28,12 @@ func main() {
 	// with generated saw scripts based on the verification template and
 	// different values of `mres` and `res_mres`. Each of these parameters can
 	// be anything in the range [0, 15], but for now, we only check a subset of
-	// all possible mres/res_mres values.
+	// all possible mres/res_mres values. In particular, we check the following
+	// (mres, res_mres) pairs:
+	//
+	// (0, 0), (1, 0)
 	mres_values := [2]int{0, 1}
-	res_mres_values := [2]int{0, 15}
+	res_mres_value := 0
 	var wg sync.WaitGroup
 	process_count := 0
 
@@ -38,14 +41,12 @@ func main() {
 	num_parallel_process := int(math.Floor((float64(total_memory) / float64(memory_used_per_test))))
 	log.Printf("System has %d bytes of memory, running %d jobs in parallel", total_memory, num_parallel_process)
 	for i := 0; i < 2; i++ {
-		for j := 0; j < 2; j++ {
-			wg.Add(1)
-			saw_template := "verify-AES-GCM-selectcheck-template.txt"
-			mres_placeholder_name := "TARGET_MRES_PLACEHOLDER"
-			res_mres_placeholder_name := "TARGET_RES_MRES_PLACEHOLDER"
-			go utility.CreateAndRunSawScript(saw_template, []string{}, mres_placeholder_name, mres_values[i], res_mres_placeholder_name, res_mres_values[j], &wg)
-			utility.Wait(&process_count, num_parallel_process, &wg)
-		}
+		wg.Add(1)
+		saw_template := "verify-AES-GCM-selectcheck-template.txt"
+		mres_placeholder_name := "TARGET_MRES_PLACEHOLDER"
+		res_mres_placeholder_name := "TARGET_RES_MRES_PLACEHOLDER"
+		go utility.CreateAndRunSawScript(saw_template, []string{}, mres_placeholder_name, mres_values[i], res_mres_placeholder_name, res_mres_value, &wg)
+		utility.Wait(&process_count, num_parallel_process, &wg)
 	}
 
 	wg.Wait()
